051a86dc6caa22542c2e8a022d2ddf71fb3fa5fe
madhav singh <madhav201singh@gmail.com>
2023-09-05 10:06:54 +0530

    update-todo

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,17 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-  todo: string[] = [];
-  task: string = '';
-
-  handleSubmit(): void {
-    if (this.task.trim() !== '') {
-      this.todo.push(this.task); // Add the new task to the todo array
-      this.task = ''; // Clear the input field
+  // let's add logic to the app component, don't worry about css, 
+  // let's focus on the logic....
+  todo:string []=[];
+  task:string = '';
+  handleSubmit():void{
+    if(this.task.trim()!==''){
+      this.todo.push(this.task);// Add the new task to the list
+      this.task='';// clear the input field.
     }
   }
-
-  handleDelete(index: number): void {
-    this.todo.splice(index, 1); // Remove the task at the specified index
+  handleDelete(index:number):void{
+    this.todo.splice(index,1); // Remove the task at the specified index....
   }
 }
+// let's add logic to the form...
+// Thanks for watching .....
